fbdfd08ba3a615f267b24bf95d50b8ddf17e0b48
Aaron Caldwell <arcaldwell49@gmail.com>
2020-09-02 12:56:49 -0400

    New pilot data added

--- PlanningAnalyses/analyze_pilotdata_v1.R
+++ PlanningAnalyses/analyze_pilotdata_v1.R
@@ -2,18 +2,18 @@
 library(readxl)
 library(tidyverse)
 library(irr)
-pilotv1 <- read_excel("pilotv1.xlsx") %>%
+pilotv1 <- read_excel("pilotv2.xlsx") %>%
   mutate(a_doi = paste0(tolower(author),"_",doi)) %>%
   mutate(id = str_replace(id," ","_")) %>%
   mutate(di_sup = ifelse(support == "Not supported", "N","Y")) %>% 
   mutate_if(is.character,as.factor)
 
-
+# put data in wide format for agreement checks
 hypo_wide = pilotv1 %>%
   select(id,a_doi,hypo_tested) %>%
   spread(id, hypo_tested) %>%
   janitor::clean_names() %>%
-  select(-na) %>%
+  #select(-na) %>%
   column_to_rownames(var = "a_doi")
 
 irr::kappam.fleiss(hypo_wide)
@@ -22,11 +22,21 @@ support_wide = pilotv1 %>%
   select(id,a_doi,di_sup) %>%
   spread(id, di_sup) %>%
   janitor::clean_names() %>%
-  select(-na) %>%
+  # select(-na) %>%
   column_to_rownames(var = "a_doi") %>%
   drop_na()
 
 irr::kappam.fleiss(support_wide)
+
+support_wide2 = pilotv1 %>%
+  select(id,a_doi,support) %>%
+  spread(id, support) %>%
+  janitor::clean_names() %>%
+  # select(-na) %>%
+  column_to_rownames(var = "a_doi") %>%
+  drop_na()
+
+irr::kappam.fleiss(support_wide2)
 
 Mode <- function(x) {
   ux <- unique(x)
@@ -40,7 +50,4 @@ df_pilot = pilotv1 %>%
   summarize(across(everything(), Mode),
             .groups = 'drop')
 
-df_test = data.frame(a = c(rep("a",7),rep("b",8)),
-                     b = c(rep("a",7),rep("b",8)),
-                     c = c(rep("a",7),rep("b",7),"a"))
-irr::kappam.fleiss(df_test)
+
